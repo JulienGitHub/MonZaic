8c650a6dc48152f9f8bdd85e18a47e2cb476a241
JulienGitHub <julien.hyra@gmail.com>
2021-09-20 15:32:46 +0200

    Add files via upload
    Faster: Added multiprocessing + xsPixel 1x1

--- mozaic.py
+++ mozaic.py
@@ -1,78 +1,111 @@
-import sys
 import os
-from PIL import Image, ImageOps
+import sys
+from PIL import Image
 from time import time, sleep
 import glob
+import time
+from multiprocessing import Process, Queue
+
+sourceToMozaicSize = Image
 
 #size of the loaded pixels in the mozaic
 pixelSize = 50
 
-def composeMozaic(file, bigPixelsArray):
-	sleep(5) #in case it is locked by os during copy
-	sourceImg = Image.open(file)
+def PutXLPixel(data, results, xsPixelsArray, sourceToMozaicSize, totalPix):
+	while True:
+		try:
+			x, y = data.get(True)
+			if x == -1:
+				break
+			rect = (x * pixelSize, y * pixelSize, (x + 1) * pixelSize, (y + 1) * pixelSize)
+			subDataOri = sourceToMozaicSize.crop(rect)
+			subData = subDataOri.resize((1, 1), Image.ANTIALIAS)
 
-	#mozaic is zoomValue times bigger than input
+			best_index = None
+			min = 2147483647 #max of 32bit int
+			current_index = 0
+			for xsPixel in xsPixelsArray:
+				color_distance = ((subData.getdata()[0][0] - xsPixel[0])**2 + (subData.getdata()[0][1] - xsPixel[1])**2 + (subData.getdata()[0][2] - xsPixel[2])**2)
+				if color_distance < min:
+					min = color_distance
+					best_index = current_index
+				current_index += 1
+			results.put((best_index, rect))
+		except:
+			pass
+	results.put((-1, (0, 0)))
+
+def composeMozaic(file, xlPixelsArray, xsPixelsArray):
+	sleep(5) #in case it is locked by os during copy
+	start_time = time.time()
+	sourceImg = Image.open(file)
+	
+	#mozaic is [zoomValue] times bigger than input
 	zoomValue = 20
+	global sourceToMozaicSize
 	sourceToMozaicSize = sourceImg.resize((sourceImg.size[0] * zoomValue, sourceImg.size[1] * zoomValue), Image.ANTIALIAS)
 	mosaic = Image.new(sourceImg.mode, (sourceImg.size[0] * zoomValue, sourceImg.size[1] * zoomValue))
 
 	nbBigPixelsWidth = int(mosaic.size[0] / pixelSize)
 	nbBigPixelsHeight = int(mosaic.size[1] / pixelSize)
 
-	smallPixWidth = int(sourceImg.size[0]/nbBigPixelsWidth)
-	smallPixHeight = int(sourceImg.size[1]/nbBigPixelsHeight)
-	smallPixelsArray = []
-	for bigPixel in bigPixelsArray:
-		smallPixelsArray.append(bigPixel.resize((smallPixWidth, smallPixHeight), Image.ANTIALIAS).convert('RGB'))
+	nbProcesses = 4
+	data = Queue(nbProcesses)	
+	results = Queue()
+	
+	for n in range(nbProcesses):
+		Process(target=PutXLPixel, args=(data, results, xsPixelsArray, sourceToMozaicSize, nbBigPixelsWidth*nbBigPixelsHeight)).start()
 
-	for x in range(nbBigPixelsWidth):
-		print str(x+1) + "/" + str(nbBigPixelsWidth);
+	for x in range(nbBigPixelsWidth):	
 		for y in range(nbBigPixelsHeight):
-			rect = (x * pixelSize, y * pixelSize, (x + 1) * pixelSize, (y + 1) * pixelSize)
+			data.put((x, y))
+	for n in range(nbProcesses):
+		data.put((-1, -1))
 
-			subDataOri = sourceToMozaicSize.crop(rect)
-			subData = subDataOri.resize((smallPixWidth, smallPixHeight), Image.ANTIALIAS)
-
-			best_index = None
-			min = 2147483647 #max of 32bit int
-			current_index = 0
-
-			for smallPixel in smallPixelsArray:
-
-				color_distance = 0
-				for i in range(len(subData.getdata())):
-					color_distance += ((subData.getdata()[i][0] - smallPixel.getdata()[i][0])**2 + (subData.getdata()[i][1] - smallPixel.getdata()[i][1])**2 + (subData.getdata()[i][2] - smallPixel.getdata()[i][2])**2)
-					if color_distance > min:
-						i = len(subData.getdata());
-				if color_distance < min:
-					min = color_distance
-					best_index = current_index
-				current_index += 1
-			mosaic.paste(bigPixelsArray[best_index], rect)
-
+	active_processes = nbProcesses
+	while True:
+		xlIndex, rect = results.get(True)
+		if xlIndex == -1:
+			active_processes -= 1
+			if active_processes <= 0:
+				break
+		else:
+			mosaic.paste(xlPixelsArray[xlIndex], rect)
+	
 	#create the output name
 	outputFile = file.replace("input", "output")
+	print("Saving Mozaic to " + outputFile)
 	mosaic.save(outputFile)
-	print outputFile + " saved"
+	print(outputFile + " saved")
+	print("--- %s seconds ---" % (time.time() - start_time))
 
 def StartMosaicThread(inputDirectory, pixelsDirectory):
 	#loading the pictures to compose the mozaics
-	bigPixelsArray = []
-	for file in glob.glob(pixelsDirectory+"\*"):
+	xlPixelsArray = []
+	xsPixelsArray = []
+
+	files = glob.glob(pixelsDirectory+"\*")
+	count = 0
+	for file in files:
 		img = Image.open(file)
+		print("Loading " + str(count+1) + "/" + str(len(files)) + " - " + file, end="\r", flush=True)
 		#let's take only the top part of the card, not the half bottom text
 		img = img.crop((180, 30, 840, 690))
-		bigPixelsArray.append(img.resize((int(pixelSize), int(pixelSize)), Image.ANTIALIAS).convert('RGB'))
-	print "Pixels Loaded"
+		xlPixelsArray.append(img.resize((int(pixelSize), int(pixelSize)), Image.ANTIALIAS).convert('RGB'))
+		pixel = img.resize((1, 1), Image.ANTIALIAS).convert('RGB').getdata()[0]		
+		xsPixelsArray.append(pixel)
+		count += 1
+	print()
+	print(str(len(xsPixelsArray)) + " Pixels Loaded")
+
 	while True:
 		#find all the png files in the input directory
 		for file in glob.glob(inputDirectory+"\*.png"):
-			composeMozaic(file, bigPixelsArray)
+			composeMozaic(file, xlPixelsArray, xsPixelsArray)
 			#delete the input image
 			os.remove(file)
 		#sleep 5 seconds before looking again
 		sleep(5)
-		
 
 if __name__ == '__main__':
 	#Calling the loop which takes in the png from the input directory
